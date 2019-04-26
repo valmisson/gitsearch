8059b5c953cf2da13a2604fcdba9e68287466916
Valmisson Grizorte <missongrizorte@gmail.com>
2019-04-26 15:06:30 -0300

    installing flexgrid.css, normalize.css and font

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,15 @@
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './routes'
 import store from './store'
 
+// CSS
+import './assets/css/normalize.css'
+import './assets/css/flexgrid.css'
+
 Vue.config.productionTip = false
 
 new Vue({
